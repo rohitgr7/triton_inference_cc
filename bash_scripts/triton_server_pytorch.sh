353a179f7208e28185b4cd4a10a7cb11e473e115
Rohit Gupta <rohitgr1998@gmail.com>
2023-02-05 19:34:49 +0530

    Sep tensorflow (#3)

---
+++ bash_scripts/triton_server_pytorch.sh
@@ -0,0 +1,1 @@
+docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 --rm -it -v ${PWD}/models/:/project/models/ -v ${PWD}/weights/:/project/weights/ triton_cc_pt:0.0.1 tritonserver --model-repository models/ --log-verbose=1 --model-control-mode=poll
